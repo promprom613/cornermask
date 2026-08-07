84d4d0ea017b178b11d72ce06bd3aa3f13624408
promprom613 <yassineharrouch209@gmail.com>
2026-08-07 09:18:51 +0100

    Update CornerMaskView.kt

--- app/src/main/java/com/prom3x209/cornermask/CornerMaskView.kt
+++ app/src/main/java/com/prom3x209/cornermask/CornerMaskView.kt
@@ -11,13 +11,20 @@ class CornerMaskView(context: Context, private val corner: Int, private val radi
 
     private val blackPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply { color = Color.BLACK }
 
+    init {
+        setLayerType(LAYER_TYPE_SOFTWARE, null)
+        setBackgroundColor(Color.TRANSPARENT)
+    }
+
     override fun onDraw(canvas: Canvas) {
         val r = radiusPx.toFloat()
+        val w = width.toFloat()
         val h = height.toFloat()
-        val cy = if (corner == 0) r else h - r
+        val cx = if (corner == 0 || corner == 2) r else w - r
+        val cy = if (corner == 0 || corner == 1) r else h - r
 
-        val fullSquare = Path().apply { addRect(0f, 0f, r, h, Path.Direction.CW) }
-        val circle = Path().apply { addCircle(r, cy, r, Path.Direction.CW) }
+        val fullSquare = Path().apply { addRect(0f, 0f, w, h, Path.Direction.CW) }
+        val circle = Path().apply { addCircle(cx, cy, r, Path.Direction.CW) }
         fullSquare.op(circle, Path.Op.DIFFERENCE)
 
         canvas.drawPath(fullSquare, blackPaint)
