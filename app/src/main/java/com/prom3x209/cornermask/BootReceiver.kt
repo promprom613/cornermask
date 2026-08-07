32a7c682ba4e7e066d90eb7c33a8753daeb4fa98
promprom613 <yassineharrouch209@gmail.com>
2026-08-07 08:28:13 +0100

    Create BootReceiver.kt

---
+++ app/src/main/java/com/prom3x209/cornermask/BootReceiver.kt
@@ -0,0 +1,21 @@
+package com.prom3x209.cornermask
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action != Intent.ACTION_BOOT_COMPLETED) return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(context)) return
+
+        val svc = Intent(context, CornerMaskService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(svc)
+        } else {
+            context.startService(svc)
+        }
+    }
+}
